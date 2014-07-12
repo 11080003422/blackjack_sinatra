ca832e6561d771ce054ff2d4b3d20a56e1e328cc
11080003422 <11080003422@ogrenci.ege.edu.tr>
2014-07-12 20:11:18 +0300

    Request username

--- main.rb
+++ main.rb
@@ -4,4 +4,21 @@ require 'sinatra/reloader' if development?
 
 set :sessions, true
 
+get '/' do
+  if session[:username].to_s == ''
+    redirect '/set_name'
+  else
+    redirect '/game'
+  end
+end
+
+get '/set_name' do
+  erb :set_name
+end
+
+post '/set_name' do
+  session[:username] = params[:name]
+  redirect '/'
+end
+
 
